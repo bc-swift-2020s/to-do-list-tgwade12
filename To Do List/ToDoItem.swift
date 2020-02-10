ec851911f2b06280c057688af1ba2fc4ec4d32dd
Thomas Wade <wadetc@bc.edu>
2020-02-10 11:45:24 -0500

    5.6 complete

---
+++ To Do List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  To Do List
+//
+//  Created by Thomas Wade on 2/10/20.
+//  Copyright © 2020 Thomas Wade. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
